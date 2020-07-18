308ece2d58d60d2831f92d9883c0c80a064728ed
Andrei Voicu <dreivoicu@gmail.com>
2020-07-18 22:05:12 +0300

    movie detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
-import { SearchComponent } from './pages/search/search.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HomeComponent } from '@pages/home/home.component';
+import { SearchComponent } from '@pages/search/search.component';
+import { MovieComponent } from '@pages/movie/movie.component';
+import { NotFoundComponent } from '@pages/not-found/not-found.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,10 @@ const routes: Routes = [
   {
     path: 'search',
     component: SearchComponent
+  },
+  {
+    path: 'movie/:id',
+    component: MovieComponent
   },
   {
     path: '**',
