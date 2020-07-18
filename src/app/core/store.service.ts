1bc20bc3a3cceabbbcba5008a630bad444375919
Andrei Voicu <dreivoicu@gmail.com>
2020-07-18 15:11:22 +0300

    refactor store

--- src/app/core/store.service.ts
+++ src/app/core/store.service.ts
@@ -1,16 +1,20 @@
 import { MoviesAdapterService } from './movies-adapter.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { APIResponse } from './api.model';
 import { map } from 'rxjs/operators';
 
+interface KeyValue {
+  [key: string]: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
-  private readonly apiUrl = 'https://api.themoviedb.org/3';
+  private readonly apiUrl = 'https://api.themoviedb.org/3/';
   private readonly apiKey = '3a6c2fcb54d6eb1f29237519e40e8089';
 
   popularMovies$: Observable<APIResponse>;
@@ -19,20 +23,29 @@ export class StoreService {
     private readonly http: HttpClient,
     private readonly adapter: MoviesAdapterService
   ) {
-    const popularUrl = `${this.apiUrl}/movie/popular?api_key=${this.apiKey}`;
-
-    this.popularMovies$ = this.get(popularUrl);
+    this.popularMovies$ = this.get('movie/popular');
   }
 
-  searchMovies(query = ''): Observable<APIResponse> {
-    const searchUrl = `${this.apiUrl}/search/movie?api_key=${this.apiKey}&query='${query}'`;
+  searchMovies(query: string): Observable<APIResponse> {
+    const params: KeyValue[] = [];
+    if (query) {
+      params.push({ query });
+    }
 
-    return this.get(searchUrl);
+    return this.get('search/movie', params);
   }
 
-  private get(url: string): Observable<APIResponse> {
+  private get(
+    endpoint: string,
+    queryParams?: KeyValue[]
+  ): Observable<APIResponse> {
+    const params = new HttpParams().set('api_key', this.apiKey);
+    if (queryParams) {
+      queryParams.forEach(({ key, value }) => params.set(key, value));
+    }
+
     return this.http
-      .get(url)
-      .pipe(map((response: any) => this.adapter.adapt(response)));
+      .get(`${this.apiUrl}${endpoint}`, { params })
+      .pipe(map((response) => this.adapter.adapt(response)));
   }
 }
