8aa5150f36e325804c295bf95568430d5e5ce55a
Andrei Voicu <dreivoicu@gmail.com>
2020-07-20 21:04:35 +0300

    added loader

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -4,10 +4,21 @@ import { CommonModule } from '@angular/common';
 import { FavoriteButtonComponent } from './favorite-button/favorite-button.component';
 import { LazySrcDirective } from './lazy-src.directive';
 import { CardImgComponent } from './card-img/card-img.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [FavoriteButtonComponent, LazySrcDirective, CardImgComponent],
+  declarations: [
+    FavoriteButtonComponent,
+    LazySrcDirective,
+    CardImgComponent,
+    LoaderComponent
+  ],
   imports: [CommonModule],
-  exports: [FavoriteButtonComponent, LazySrcDirective, CardImgComponent]
+  exports: [
+    FavoriteButtonComponent,
+    LazySrcDirective,
+    CardImgComponent,
+    LoaderComponent
+  ]
 })
 export class SharedModule {}
--- src/app/pages/movie/to-movie.pipe.ts
+++ src/app/pages/movie/to-movie.pipe.ts
@@ -1,13 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-import { movieDetailToMovie } from '@core/adapter/utils';
 import { MovieDetail, Movie } from '@core/api.model';
 
 @Pipe({
   name: 'toMovie'
 })
 export class ToMoviePipe implements PipeTransform {
-  transform(value: MovieDetail): Movie {
-    return movieDetailToMovie(value);
+  transform(movieDetail: MovieDetail): Movie {
+    return new Movie(
+      movieDetail.id,
+      movieDetail.vote,
+      movieDetail.title,
+      movieDetail.overview,
+      movieDetail.posterPath
+    );
   }
 }
